0b140f8b73d0924b8d44a28eb9193187692203f0
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-09 00:52:57 +0200

    handing req depending on selected post status

--- src/hooks/useGetPosts.tsx
+++ src/hooks/useGetPosts.tsx
@@ -1,22 +1,27 @@
 import axios from "axios";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { PostItem, PostStatusType } from "../types";
 
-export interface DataItem {
-  id: number;
-  title: string;
-  body: string;
-  topRate: boolean;
-  status: "publish" | "draft" | "blocked";
-}
-const fetchPosts = async (): Promise<DataItem[]> => {
-  const res = await axios.get<DataItem[]>("http://localhost:3009/posts");
-  return res.data;
+const fetchPosts = async (
+  selectedStatus: PostStatusType
+): Promise<PostItem[]> => {
+  if (selectedStatus === "all") {
+    const res = await axios.get<PostItem[]>("http://localhost:3009/posts");
+    return res.data;
+  } else {
+    const res = await axios.get<PostItem[]>(
+      `http://localhost:3009/posts?status=${selectedStatus}`
+    );
+    return res.data;
+  }
 };
 
-const useGetPosts = (): UseQueryResult<DataItem[]> => {
+const useGetPosts = (
+  selectedStatus: PostStatusType
+): UseQueryResult<PostItem[]> => {
   const query = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts", { selectedStatus }],
+    queryFn: () => fetchPosts(selectedStatus),
     staleTime: 1000 * 10, // 10 seconds
   });
   return query;
