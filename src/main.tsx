6615a92df8da057871039bfceb85c3514bd19025
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-08 23:46:48 +0200

    adding devtools to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -34,5 +35,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
