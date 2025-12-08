b4315ada9f435587178e7a02c3b01a6c7e25597e
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-08 23:26:27 +0200

    creating an external post compnent

---
+++ src/components/Post.tsx
@@ -0,0 +1,38 @@
+import { DataItem } from "../hooks/useGetPosts";
+import { Link } from "react-router-dom";
+import { Form, ButtonGroup, Button } from "react-bootstrap";
+
+interface PostProps {
+  post: DataItem;
+  index: number;
+}
+
+const Post = ({ post, index }: PostProps) => {
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Handle the change event for the switch
+    console.log(`Post ${post.id} topRate changed to ${e.target.checked}`);
+  };
+  return (
+    <tr>
+      <td>{++index}</td>
+      <td>
+        <Link to="/info">{post.title}</Link>
+      </td>
+      <td>{post.status}</td>
+      <td style={{ textAlign: "center" }}>
+        <Form.Check // prettier-ignore
+          type="switch"
+          checked={post.topRate}
+          onChange={onChangeHandler}
+        />
+      </td>
+      <td>
+        <ButtonGroup aria-label="Basic example">
+          <Button variant="danger">Delete</Button>
+        </ButtonGroup>
+      </td>
+    </tr>
+  );
+};
+
+export default Post;
