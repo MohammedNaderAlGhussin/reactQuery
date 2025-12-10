81a9aba278036f4dc99382aa139e4ec9a8b43cd0
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-10 22:09:58 +0200

    Abortting unwanted request.

--- src/hooks/useGetComments.ts
+++ src/hooks/useGetComments.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 import { CommentResponse } from "../types";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 
-const fetchComments = async (post_id: string): Promise<CommentResponse[]> => {
+const fetchComments = async (
+  post_id: string,
+  signal: AbortSignal
+): Promise<CommentResponse[]> => {
   const res = await axios.get<CommentResponse[]>(
-    `http://localhost:3009/comments?post_id=${post_id}&_sort=id&_order=desc`
+    `http://localhost:3009/comments?post_id=${post_id}&_sort=id&_order=desc`,
+    { signal }
   );
   return res.data;
 };
@@ -12,7 +16,7 @@ const fetchComments = async (post_id: string): Promise<CommentResponse[]> => {
 const useGetComments = (post_id: string): UseQueryResult<CommentResponse[]> => {
   const query = useQuery({
     queryKey: ["comments", { post_id: +post_id }],
-    queryFn: () => fetchComments(post_id),
+    queryFn: ({ signal }) => fetchComments(post_id, signal),
   });
   return query;
 };
