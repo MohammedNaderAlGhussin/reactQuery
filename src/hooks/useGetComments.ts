243cc39a8bb1d3966ba30e1c45017fad09a71e01
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-10 19:30:04 +0200

    adding a custom hook for retrieving post's comments

---
+++ src/hooks/useGetComments.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { CommentResponse } from "../types";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+
+const fetchComments = async (postId: string): Promise<CommentResponse[]> => {
+  const res = await axios.get<CommentResponse[]>(
+    `http://localhost:3009/comments?post_id=${postId}&_sort=id&_order=desc`
+  );
+  return res.data;
+};
+
+const useGetComments = (postId: string): UseQueryResult<CommentResponse[]> => {
+  const query = useQuery({
+    queryKey: ["comments", postId], 
+    queryFn: () => fetchComments(postId),
+    staleTime: 1000 * 10, // 10 seconds
+  });
+  return query;
+};
+
+export default useGetComments;
